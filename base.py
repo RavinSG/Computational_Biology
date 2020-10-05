7d80d708216ff3aec1add5f4a8df426332d0f689
ravin <ravinsg@gmail.com>
2020-10-05 10:14:06 +0530

    feat: added finding frequent words with mismatches

--- base.py
+++ base.py
@@ -1,4 +1,6 @@
+import numpy as np
 from collections import defaultdict, Counter
+from itertools import combinations, product
 
 NUCLEOTIDES = ['A', 'T', 'C', 'G']
 COMPLIMENTS = {'A': 'T', 'T': 'A', 'G': 'C', 'C': 'G'}
@@ -55,3 +57,63 @@ def pattern_matching(strand, pattern):
 
 def transcription(strand):
     return strand.replace('T', 'U')
+
+
+def hamming_distance(strand1, strand2):
+    distance = 0
+    for i in range(len(strand1)):
+        if strand1[i] != strand2[i]:
+            distance += 1
+
+    return distance
+
+
+def skew_loc(strand, loc='min'):
+    bases = {'A': 0, 'T': 0, 'C': -1, 'G': 1}
+    values = [0]
+
+    for x, y in enumerate(strand):
+        values.append(values[x] + bases[y])
+
+    values = np.array(values)
+    if loc == 'min':
+        return np.where(values == values.min())[0]
+    else:
+        return np.where(values == values.max())[0]
+
+
+def approximate_pattern_count(strand, pattern, d):
+    s_l = len(strand)
+    p_l = len(pattern)
+    count = 0
+    for i in range(s_l - p_l + 1):
+        splice = strand[i: i + p_l]
+        if (hamming_distance(splice, pattern)) <= d:
+            count += 1
+
+    return count
+
+
+def get_neighbors(k_mer, d):
+    mismatches = [k_mer]
+    alt_bases = {'A': 'CGT', 'C': 'AGT', 'G': 'ACT', 'T': 'ACG'}
+    for dist in range(1, d + 1):
+        for change_indices in combinations(range(len(k_mer)), dist):
+            for substitutions in product(*[alt_bases[k_mer[i]] for i in change_indices]):
+                new_mismatch = list(k_mer)
+                for idx, sub in zip(change_indices, substitutions):
+                    new_mismatch[idx] = sub
+                mismatches.append(''.join(new_mismatch))
+    return mismatches
+
+
+def frequent_words_with_mismatch(strand, k, d):
+    possible_k_mers = defaultdict(int)
+    for i in range(len(strand) - k):
+        splice = strand[i: i + k]
+        neighbours = get_neighbors(splice, d)
+        for neighbour in neighbours:
+            possible_k_mers[neighbour] += 1
+
+    max_count = max(possible_k_mers.values())
+    return [x for x, y in possible_k_mers.items() if y == max_count]
