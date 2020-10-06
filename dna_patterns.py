28ec49c626d3b61aeb7ef88298510940eb13a281
ravin <ravinsg@gmail.com>
2020-10-06 20:30:20 +0530

    feat: add median string finding method

--- dna_patterns.py
+++ dna_patterns.py
@@ -1,5 +1,6 @@
 import numpy as np
 from collections import Counter
+from dna_replication import hamming_distance, number_to_pattern
 
 
 def generate_motif_profile(motifs):
@@ -15,9 +16,45 @@ def generate_motif_profile(motifs):
         profile.append(temp)
 
     profile = np.array(profile).transpose()
-    return profile / len(motifs)
+    return [profile / len(motifs), sum(motifs.shape[0] - profile.max(axis=0))]
+
+
+def consensus(motifs):
+    mapping = {0: 'A', 1: 'C', 2: 'G', 3: 'T'}
+    profile = generate_motif_profile(motifs)[0]
+    return "".join([mapping[x] for x in np.argmax(profile, axis=0)])
 
 
 def calculate_entropy(motif_profile):
     entropy_values = np.nan_to_num(np.log2(motif_profile) * motif_profile)
     return entropy_values.sum()
+
+
+def strand_score(strand, k_mer):
+    length = len(k_mer)
+    scores = []
+    for i in range(len(strand) - length + 1):
+        scores.append(hamming_distance(strand[i:i + length], k_mer))
+
+    return min(scores)
+
+
+def calculate_score(strands, k_mer):
+    final_score = 0
+    for strand in strands:
+        final_score += strand_score(strand, k_mer)
+
+    return final_score
+
+
+def median_string(strands, k):
+    d = np.inf
+    patterns = []
+    for i in range(4 ** k):
+        pattern = number_to_pattern(i, k)
+        score = calculate_score(strands, pattern)
+        if d > score:
+            patterns.append(pattern)
+            d = score
+
+    return patterns
