f5821d0637b6d5b3d0c0a08fe346892ec560d46d
ravin <ravinsg@gmail.com>
2020-10-06 20:29:27 +0530

    refactor: remove base file and move the constants to params.py

---
+++ params.py
@@ -0,0 +1,8 @@
+NUCLEOTIDES = ['A', 'T', 'C', 'G']
+COMPLIMENTS = {'A': 'T', 'T': 'A', 'G': 'C', 'C': 'G'}
+SWAP_BASES = {'A': 'CGT', 'C': 'AGT', 'G': 'ACT', 'T': 'ACG'}
+
+GC_SCORE = {'A': 0, 'T': 0, 'C': -1, 'G': 1}
+
+STR_TO_NUM = {'A': 0, 'C': 1, 'G': 2, 'T': 3}
+NUM_TO_STR = {0: 'A', 1: 'C', 2: 'G', 3: 'T'}
