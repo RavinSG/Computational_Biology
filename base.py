27ec9a7b665d7ff685e8e937d06e5645f21f8091
ravin <ravinsg@gmail.com>
2020-10-06 08:52:54 +0530

    feat: added generation of motif profiles.

--- base.py
+++ base.py
@@ -151,3 +151,24 @@ def motif_enumeration(strands, k, d):
                 patterns.append(neighbour)
 
     return set(patterns)
+
+
+def generate_motif_profile(motifs):
+    profile_loc = {'A': 0, 'C': 1, 'G': 2, 'T': 3}
+    motifs = np.array([list(i) for i in motifs])
+    profile = []
+
+    for i in range(motifs.shape[1]):
+        c = Counter(motifs[:, i])
+        temp = [0] * 4
+        for key, val in c.items():
+            temp[profile_loc[key]] = val
+        profile.append(temp)
+
+    profile = np.array(profile).transpose()
+    return profile / len(motifs)
+
+
+def calculate_entropy(motif_profile):
+    entropy_values = np.nan_to_num(np.log2(motif_profile) * motif_profile)
+    return entropy_values.sum()
