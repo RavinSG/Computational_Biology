651f8b5bbd99777000e95aea8af0be1965a35583
ravin <ravinsg@gmail.com>
2020-10-07 00:26:21 +0530

    feat: implement greedy motif search

--- dna_patterns.py
+++ dna_patterns.py
@@ -36,13 +36,13 @@ def strand_score(strand, k_mer):
     for i in range(len(strand) - length + 1):
         scores.append(hamming_distance(strand[i:i + length], k_mer))
 
-    return min(scores)
+    return [min(scores), np.argmin(scores)]
 
 
 def calculate_score(strands, k_mer):
     final_score = 0
     for strand in strands:
-        final_score += strand_score(strand, k_mer)
+        final_score += strand_score(strand, k_mer)[0]
 
     return final_score
 
@@ -58,3 +58,37 @@ def median_string(strands, k):
             d = score
 
     return patterns
+
+
+def most_probable_k_mer(strand, k, profile):
+    profile = np.array(profile)
+    probable_k_mer = strand[:k]
+    max_prob = 0
+    for i in range(0, len(strand) - k + 1):
+        splice = strand[i:i + k]
+        indices = [STR_TO_NUM[x] for x in splice]
+        prob = np.prod(profile[[indices], range(len(indices))])
+        if prob > max_prob:
+            max_prob = prob
+            probable_k_mer = splice
+
+    return probable_k_mer
+
+
+def greedy_motif_search(strands, k, t):
+    best_motifs = [x[:k] for x in strands]
+    best_score = generate_motif_profile(best_motifs)[-1]
+    strand_1 = strands[0]
+    for i in range(len(strands[0]) - k + 1):
+        splice = strand_1[i:i + k]
+        candidate_motifs = [splice]
+        for strand in strands[1:]:
+            profile, _ = generate_motif_profile(candidate_motifs)
+            best_k_mer = most_probable_k_mer(strand, k, profile)
+            candidate_motifs.append(best_k_mer)
+        candidate_score = generate_motif_profile(candidate_motifs)[-1]
+        if best_score > candidate_score:
+            best_score = candidate_score
+            best_motifs = candidate_motifs
+
+    return best_motifs
