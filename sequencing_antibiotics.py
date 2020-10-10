8c5cc53a1464ec882384c6a8dd9b4acbcf8281db
ravin <ravinsg@gmail.com>
2020-10-10 14:15:50 +0530

    feat: generation of theoretical spectrum and calculation of the possible number of peptides given a mass.

--- sequencing_antibiotics.py
+++ sequencing_antibiotics.py
@@ -1,5 +1,6 @@
-from params import *
+from math import log, exp
 
+from params import *
 from utilities import transcription
 from dna_replication import get_complement
 
@@ -17,14 +18,58 @@ def protein_translation(rna_strand):
 
 def peptide_encoding_substring(strand, peptide):
     str_len = 3 * len(peptide)
-
+    substrings = []
     for i in range(len(strand) - str_len + 1):
         substring = strand[i:i + str_len]
         complement = get_complement(substring, reverse=True)
 
         for s in [substring, complement]:
             rna = transcription(s)
-            print(protein_translation(rna))
+            if protein_translation(rna) == peptide:
+                substrings.append(substring)
+                break
+    return substrings
 
 
-peptide_encoding_substring("ATGGCCAT", "MA")
+def theoretical_spectrum(peptide):
+    cyclospectrum = ["", peptide]
+    weights = [0, sum([INTEGER_MASS[x] for x in peptide])]
+
+    l_peptide = len(peptide)
+    peptide = peptide * 2
+
+    for i in range(1, l_peptide):
+        for j in range(l_peptide):
+            codons = peptide[j:j + i]
+            weights.append(sum([INTEGER_MASS[x] for x in codons]))
+            cyclospectrum.append(codons)
+
+    weights, cyclospectrum = zip(*sorted(zip(weights, cyclospectrum), key=lambda pair: pair[0]))
+
+    return weights, cyclospectrum
+
+
+def num_of_possible_peptides(mass, mass_dict):
+    if mass == 0:
+        return 1, mass_dict
+    elif mass < 57:
+        return 0, mass_dict
+    elif mass in mass_dict:
+        return mass_dict[mass], mass_dict
+    else:
+        n = 0
+        for m in DISTINCT_MASSES:
+            i, mass_dict = num_of_possible_peptides(mass - m, mass_dict)
+            n += i
+        mass_dict[mass] = n
+
+        return n, mass_dict
+
+
+def calculate_c(num_1, num_2):
+    i_1 = num_of_possible_peptides(num_1, {})[0]
+    i_2 = num_of_possible_peptides(num_2, {})[0]
+
+    c = exp(log(i_1 / i_2) / (num_1 - num_2))
+
+    print(c)
